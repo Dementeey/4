e256c0b5a2580c784838ed0c4dd6d70e28e9ea2f
dementeeyMac <metallist9656@gmail.com>
2018-07-29 09:02:53 +0300

    save.

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
+// Variables
+const cardBtn      = document.querySelectorAll('.card__btn');
+const modal        = document.querySelector('.modal');
+const modalOverlay = document.querySelector('.modal__overlay');
+const modalCloseBtn  = document.querySelector('.card-modal__close-btn');
+
+
+// Function
+const showModal = () => {
+  console.log('click');
+
+  if ( modal.classList.contains('modal--hide') ) {
+    document.body.style.overflow = 'hidden';
+    modal.classList.remove('modal--hide');
+    return
+  }
+
+  document.body.style.overflow = '';
+  modal.classList.add('modal--hide');
+  return
+}
+
+// Events
+document.addEventListener('DOMContentLoaded', () => {
   // eslint-disable-next-line
   if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
     return document.documentElement.classList.add('touch');
@@ -6,3 +29,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
   return document.documentElement.classList.add('no-touch');
 })
+document.addEventListener('keydown', (e) => {
+  const keycode = e.keyCode || e.which;
+
+  if (keycode === 27) { // key 'Esc'
+    e.preventDefault;
+    showModal();
+  }
+})
+for (let i = 0; i < cardBtn.length; i += 1) {
+  cardBtn[i].addEventListener('click', showModal);
+}
+modalOverlay.addEventListener('click', showModal);
+modalCloseBtn.addEventListener('click', showModal);
